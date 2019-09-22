55ff56cb6e754faf30396a31de648811a03352a0
DESKTOP-UE1AEIF\marce <gicu.buruiana@gmail.com>
2019-09-22 02:24:10 -0400

    no message

--- src/components/modals/ComponentInfo/componentInfo.js
+++ src/components/modals/ComponentInfo/componentInfo.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
-import Collapse from "react-bootstrap/Collapse";
-import get from 'lodash/get';
 
 const ComponentInfo = ({ removeModal, modalData }) => {
   const { node } = modalData[0];
-  const [open, setOpen] = useState(new Map());
+  const [open, setOpen] = useState({});
 
   const renderDescription = descr => {
     return descr
@@ -17,31 +15,34 @@ const ComponentInfo = ({ removeModal, modalData }) => {
       : null;
   };
 
-  const handleCklick = title => {
-    console.log('console: -----------------', title, open);
-    console.log('console: =================', open.get(title) || false);
-    setOpen(open.set(title, !open.get(title)));
-  };
-
   const renderProps = () => {
     const propsInfo = node.componentProps;
 
-    return propsInfo.map(prop => {
-      return (
-        <div key={prop.title}>
-          ---{open}---
-          <a onClick={() => handleCklick(prop.title)} key={`${prop.title}_a`}>{prop.title}</a>
-          {
-            (
-              open.get(prop.title) || false
-            ) &&
-              <div>
-                {prop.propType}
-                {prop.description}
-              </div>
-            }
-        </div>
+    const handleCklick = title => {
+      setOpen({
+        ...open,
+        [title]: !open[title]
+      });
+    };
 
+    const renderDetails = prop => {
+      return (open[prop.title] || false)
+        ? (
+          <div>
+            {prop.propType}
+            {prop.description}
+          </div>
+        )
+        : null;
+    };
+
+    return propsInfo.map(prop => {
+      const { title } = prop;
+      return (
+        <div key={title}>
+          <a onClick={() => handleCklick(title)} key={`${title}_a`} className='propsStyle'>{title}</a>
+          {renderDetails(prop)}
+        </div>
       );
     });
   };
@@ -70,18 +71,3 @@ const ComponentInfo = ({ removeModal, modalData }) => {
 };
 
 export default ComponentInfo;
-
-
-{/* <div key={prop.name}>
-  <a href="" onClick={setOpen(!open)} >{prop.name}</a>
-  <Collapse in={open}>
-    <div>
-      {prop.propType}
-    </div>
-    <div>
-      <textarea rows={Math.round(prop.description.length / 30)} cols="118" className='textarea-noBorder'>
-        {prop.description}
-      </textarea>
-    </div>
-  </Collapse>
-</div> */}
